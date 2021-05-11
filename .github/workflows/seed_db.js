5b8c9970ae29e462fb527400c677ce072250937c
Ryan Bethel <ryan.bethel@gmail.com>
2021-05-11 16:22:25 -0400

    testing something

--- .github/workflows/seed_db.js
+++ .github/workflows/seed_db.js
@@ -12,7 +12,7 @@ pgclient.connect();
 
 const table =
     "CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))";
-const text = "INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *";
+const text = "INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) ";
 const values = [
     "Mona the",
     "Octocat",
@@ -29,8 +29,8 @@ pgclient.query(text, values, (err, res) => {
     if (err) throw err;
 });
 
-// pgclient.query("SELECT * FROM student", (err, res) => {
-//     if (err) throw err;
-//     console.log(err, res.rows); // Print the data in student table
-//     pgclient.end();
-// });
+pgclient.query("SELECT * FROM student", (err, res) => {
+    if (err) throw err;
+    console.log(err, res.rows); // Print the data in student table
+    pgclient.end();
+});
